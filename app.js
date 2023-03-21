e077831987b50959a37ac3cbcc8dc030d1d34879
msshahs <meethshah663@gmail.com>
2023-03-21 16:27:24 +0530

    two

--- app.js
+++ app.js
@@ -9,45 +9,17 @@ const PORT = 4000;
 const DB =
   "mongodb+srv://mhs_db:1234meetshah@cluster0.dg2l0.mongodb.net/mernstack?retryWrites=true&w=majority";
 
-const table_format_schema = new mongoose.Schema({
-    no_of_lecs : {
-        type:Number,
-        required:true
-    },
-    lec_duration : {
-        hour:Number,
-        minute:Number
-    },
-    no_of_days : {
-        type:Number,
-        required:true
-    },
-    recess_time : {
-        hour:Number,
-        minute:Number,
-    },
-    start_time : {
-        hour:Number,
-        minute:Number,
-        
-    },
-    leave_time : {
-        hour:Number,
-        minute:Number,
-        
-    },
-    createdAt:{
-        type:Date,
-        default:new Date(Date.now())
-    },
-    event_type:{
-        type:String,
-        required:true
-    }
+const collection2_schema = new mongoose.Schema({
+  
+        subjectName:String,
+        facultyName:String,
+        lectureTime:String,
+        lectureDuration:Number,
+        day:String
 
 })
 
-const format_model = mongoose.model('format',table_format_schema);
+const collection2 = mongoose.model('schedule',collection2_schema);
 
 
 mongoose
@@ -64,80 +36,101 @@ app.use(express.json());
 
 //default format-created 
 
-app.get('/createtableset',async(req,res,next)=>{
 
+app.get("/", (req,res) => {
+    console.log('working');
+});
 
-    const table_set = await format_model.create({
-        no_of_lecs:4,
-        lec_duration:{
-            hour:1,
-            minute:0
-        },
-        no_of_days:7,
-        recess_time:{
-            hour:1,
-            minute:0
-        },
-        start_time:{
-            hour:11,
-            minute:0
-        },
-        leave_time:{
-            hour:5,
-            minute:0
-        },
-        event_type:"afternoon_shift"
+app.get('/setschedule',async(req,res)=>{
+    const table_set = await collection2.create({
+        subjectName:"Maths",
+        facultyName:"Raj Sir",
+        lectureTime:"11:00",
+        lectureDuration:60,
+        day:"Wednesday"
     })
 
-    res.status(400).json({
+    res.status(200).json({
         success:true,
         data:table_set
     })
+
 })
-
-//for user input -create table format
-app.post('/newtable',(req,res)=>{
-
-    let lec_hour  = req.body.lec_duration.split(':')[0]
-    let lec_min  = req.body.lec_duration.split(':')[1]
-    console.log(lec_hour,lec_min);
-    let recess_hour = req.body.recess_time.split(':')[0]
-    let recess_min = req.body.recess_time.split(':')[1]
-
-    let strat_hour = req.body.start_time.split(':')[0]
-    let start_min = req.body.start_time.split(':')[1]
-
-    let leave_hour = req.body.leave_time.split(':')[0]
-    let leave_min = req.body.leave_time.split(':')[1]
-
-    let event_type = req.body.event_type;
-    let no_of_lecs = req.body.no_of_lecs;
-    
+app.post('/setrecord',async(req,res)=>{
+    const tableset = await collection2.create({    
+        subjectName:req.body.sub,
+        facultyName:req.body.fac,
+        lectureTime:req.body.time,
+        lectureDuration:req.body.duration,
+        day:req.body.day
+    })
     res.status(200).json({
         success:true,
-        msg:"table format registered successfully"
-    })
+        data:tableset
+    })  
 })
 
+app.get('/getschedule',async(req,res,next)=>{
 
-app.get('/getformat',async(req,res,next)=>{
-
-    let fetch_data = await format_model.find();
-    let len = fetch_data.length;
-    let select =Math.floor(Math.random() * len);
+    let fetch_data = await collection2.find();
 
     res.status(200).json({
         success:true,
-        data:fetch_data[select]
+        data:fetch_data
     })
 
 })
-app.get("/", (req,res) => {
-  res.send("Working");
+
+//fetch records via id
+
+app.get('/record/:id',async(req,res)=>{
+    const id = req.params.id ;
+
+    const dataCollected  = await collection2.find({_id:id});
+
+    console.log(dataCollected);
+    res.status(200).json({
+        success:true,
+        data:dataCollected
+    });
 });
 
 
-app
+
+
+//update record 
+
+app.post('/updaterecord',async(req,res)=>{
+
+    const findRecord  = await collection2.findByIdAndUpdate(req.body.id,{
+        subjectName:req.body.sub,
+        facultyName:req.body.fac,
+        lectureDuration:req.body.duration,
+    },{new:true});
+
+    res.status(200).json({
+        success:true,
+        findRecord
+    })
+
+
+})
+
+// delete a record
+
+app.post('/deleterecord',async(req,res)=>{
+    
+    const findRecord  = await collection2.findByIdAndDelete(req.body.id);
+
+    res.status(200).json({
+        success:true,
+        findRecord
+    })
+
+
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`running on port no ${PORT}`);
